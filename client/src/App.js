349af69fa2f915f75b0c94cc83983f66caf101d2
ulritter <ulritter@gmail.com>
2025-04-25 18:15:30 +0200

    d13 hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
@@ -17,15 +17,15 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Uwe Neu
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
+        <Routes>
+          <Route path="/" element={<Fib />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
       </div>
     </Router>
   );
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const keys = require("./keys");
- 
+
 // Express App Setup
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
- 
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
- 
+
 // Postgres Client Setup
 const { Pool } = require("pg");
 const pgClient = new Pool({
@@ -18,13 +18,13 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
 });
- 
+
 pgClient.on("connect", (client) => {
   client
     .query("CREATE TABLE IF NOT EXISTS values (number INT)")
     .catch((err) => console.error(err));
 });
- 
+
 // Redis Client Setup
 const redis = require("redis");
 const redisClient = redis.createClient({
@@ -32,43 +32,43 @@ const redisClient = redis.createClient({
   retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
- 
+
 (async () => {
   await redisClient.connect();
   await redisPublisher.connect();
 })();
- 
+
 // Express route handlers
- 
+
 app.get("/", (req, res) => {
   res.send("Hi");
 });
- 
+
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * from values");
-  
+
   res.send(values.rows);
 });
- 
+
 app.get("/values/current", async (req, res) => {
   const values = await redisClient.hGetAll("values");
   res.send(values);
 });
- 
+
 app.post("/values", async (req, res) => {
   const index = req.body.index;
- 
+
   if (parseInt(index) > 40) {
     return res.status(422).send("Index too high");
   }
- 
+
   await redisClient.hSet("values", index, "Nothing yet!");
   await redisPublisher.publish("insert", index);
   pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
- 
+
   res.send({ working: true });
 });
- 
+
 app.listen(5000, (err) => {
   console.log("Listening");
 });
