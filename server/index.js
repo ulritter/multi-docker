fb51b2ef6b4219bfaafcac2e27943b57dd55038b
ulritter <ulritter@gmail.com>
2025-04-25 14:32:53 +0200

    changed fib

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
  
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * from values");
- 
+  
   res.send(values.rows);
 });
  
