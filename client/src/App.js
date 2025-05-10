b0713f2d1c98efa63eeaff5600c7f3f171570129
ulritter <ulritter@gmail.com>
2025-05-10 22:35:22 +0200

    gcp 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.scss";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
@@ -10,20 +10,9 @@ function App() {
     <Router>
       <div className="App">
         <header >
-        <div>
-            <div className="cube-container">
-              <div className="cube">
-                <div className="face front">Uwe ...</div>
-                <div className="face back">Uwe ...</div>
-                <div className="face left">Uwe ...</div>
-                <div className="face right">Uwe ...</div>
-                <div className="face top"></div>
-                <div className="face bottom"></div>
-              </div>
-            </div>
-          </div>
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Fibonacci Calculator K8s (GCP)
+           Fibonacci Calculator K8s (GCP 3)
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
